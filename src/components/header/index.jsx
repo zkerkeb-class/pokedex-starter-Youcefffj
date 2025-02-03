210c90427f4be1b2bad5da4b3f8bba852ae63447
Youcefffj <rataconbigotee@gmail.com>
2025-02-03 18:15:00 +0100

    import array pokemon + jeu use de la donnée

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,11 +3,17 @@
 // const Header = ({prenom, autre}) => {
 //avec valeur par defaut pour prenom : const Header = ({prenom="default", autre}) => {
 
+import pokemons from "../../assets/pokemons";
+
 const Header = (props) => {
     console.log("Header props : ",props);
+    console.log("pokemons : ",pokemons[0]);
+    const bulbi = pokemons[0];
+    console.log("bulbi : ",bulbi.name.french);
     return (
         <header>
             <h1>Salut {props.prenom}</h1>
+            <h2>Je suis {bulbi.name.french}</h2>
         </header>
     );
 };
