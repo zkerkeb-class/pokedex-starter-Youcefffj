d96e19a959bdc8fd860fc14c7cbf3becabbee9ca
Youcefffj <rataconbigotee@gmail.com>
2025-02-05 16:46:38 +0100

    gallery + ajout de fond

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import Carte from './components/Carte'
 import pokemons from "./assets/pokemons";
 
 function App() {
-  const pokemon = pokemons[24];
+  const randomIndex = Math.floor(Math.random() * pokemons.length);
+  const pokemon = pokemons[randomIndex];
   return (
     <>
-      <div>
-        <Carte pokemon={pokemon}/>
+
+      <h1>Une carte aléatoire</h1>
+      <div className="exemple">
+        <Carte pokemon={pokemon} />
       </div>
+      <h1>Pokedex</h1>
+      <div className="gallery">
+      {pokemons.map((pokemon, index) => (
+        <Carte key={pokemon.id || index} pokemon={pokemon} />
+      ))}
+    </div>
     </>
   )
 }
